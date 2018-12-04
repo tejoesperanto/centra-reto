7bb7d999229288cc18259aedb0e03f6c4f94ddbd
Mia Nordentoft <mia@miestasmia.com>
2018-12-04 14:28:28 +0100

    If you're already logged in /ensaluti will redirect you

--- src/routers/web/_ensaluti.js
+++ src/routers/web/_ensaluti.js
@@ -1,4 +1,15 @@
+import url from 'url';
+
 async function ensaluti (req, res, next) {
+	if (req.user) {
+		const searchString = url.parse(req.url, true).search;
+		let redirectUrl = '/';
+		if (searchString) { redirectUrl = searchString.slice(1); }
+
+		res.redirect(303, redirectUrl);
+		return
+	}
+
 	await res.sendFullPage('ensaluti');
 }
 
