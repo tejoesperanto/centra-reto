e8b4136f0712c791789f4d480a630e4039da05cb
Mia Nordentoft <mia@miestasmia.com>
2019-09-16 09:11:53 +0200

    Add temp db

--- src/routers/web/vochdonado/_retaj.js
+++ src/routers/web/vochdonado/_retaj.js
@@ -1,6 +1,9 @@
 async function index (req, res, next) { // eslint-disable-line no-unused-vars
 	const data = {
-		title: 'Reta voĉdonado'
+		title: 'Reta voĉdonado',
+		scripts: [
+			'/js/cr/main/vochdonado/retaj.js',
+		]
 	};
 
 	await res.sendRegularPage('vochdonado/retaj', data);
