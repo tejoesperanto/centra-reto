5d62516abb0026e84622eaafb778930d53d05398
Mia Nordentoft <mia@tejo.org>
2019-09-23 13:53:39 +0200

    Add js to obtain data from create vote form

--- files/web/static/js/cr/main/vochdonado/retaj.js
+++ files/web/static/js/cr/main/vochdonado/retaj.js
@@ -49,10 +49,71 @@ $(function () {
 			$('#create-vote-form-numWinners,#create-vote-form-opts')
 				.prop('disabled', true)
 				.prop('required', false);
+			$('#create-vote-form-quorum,#create-vote-form-majority')
+				.prop('disabled', false)
+				.prop('required', true);
 		} else {
 			$('#create-vote-form-numWinners,#create-vote-form-opts')
 				.prop('disabled', false)
 				.prop('required', true);
+			$('#create-vote-form-quorum,#create-vote-form-majority')
+				.prop('disabled', true)
+				.prop('required', false);
 		}
+	});
+
+	$('#create-vote-form').submit(function (e) {
+		e.preventDefault();
+
+		var apiData = {
+			name: $('#create-vote-form-name').val(),
+			description: $('#create-vote-form-description').val(),
+			type: $('#create-vote-form-type').val(),
+			timeTo: $('#create-vote-form-timeTo').data('DateTimePicker').date().unix(),
+			secret: $('#create-vote-form-secret').is(':checked'),
+			groups: $('#create-vote-form-groups').tagsinput('items')
+				.map(function (x) {
+					return x.id;
+				})
+		};
+
+		if (apiData.type === 'jns') {
+			switch ($('#create-vote-form-quorum').val()) {
+			case '1/2':
+				apiData.quorum = 1/2;
+				break;
+			case '2/3':
+				apiData.quorum = 2/3;
+				break;
+			default:
+				apiData.quorum = 0;
+			}
+
+			switch ($('#create-vote-form-majority').val()) {
+			case 'plej':
+				apiData.majority = 1/2;
+				apiData.majorityMustBeGreater = true;
+				break;
+			case '2/3':
+				apiData.majority = 2/3;
+				apiData.majorityMustBeGreater = false;
+				break;
+			default:
+				apiData.majority = 0;
+				apiData.majorityMustBeGreater = false;
+			}
+		} else {
+			apiData.numWinners = parseInt($('#create-vote-form-numWinners').val(), 10);
+			apiData.opts = $('#create-vote-form-opts').val()
+				.split(/\r?\n/g)
+				.map(function (x) {
+					return x.trim();
+				})
+				.filter(function (x) {
+					return x.length > 0;
+				});
+		}
+
+		console.log(apiData);
 	});
 });
