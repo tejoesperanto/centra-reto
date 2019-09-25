14ba8b700dd000f96039e4ebdb4ab87b8ef99a20
Mia Nordentoft <mia@tejo.org>
2019-09-25 19:19:44 +0200

    Basic results generation (no tie breaker support)

--- src/api/vote.js
+++ src/api/vote.js
@@ -1,9 +1,20 @@
 import moment from 'moment-timezone';
+import { RankedPairs, STV } from 'vocho-lib';
 import { promisify } from 'util';
 import _csvParse from 'csv-parse';
 const csvParse = promisify(_csvParse);
 
 import Group from './group';
+
+const range = (start, end) => Array.from({length: (end - start + 1)}, (v, k) => k + start);
+const symbols = String.fromCharCode(
+	...range(0x30, 0x39), // 0-9
+	...range(0x41, 0x5a), // A-Z
+	...range(0x61, 0x7a), // a-z
+	...range(0xc0, 0xd6), // latin-1 uppercase 1
+	...range(0xd8, 0xf6), // latin-1 uppercase 2, latin-1 lowercase 1
+	...range(0xf8, 0xff), // latin-1 lowercase 2
+);
 
 /**
  * Gets all the votes the user can see
@@ -71,6 +82,88 @@ export async function getUserVotes (user) {
 		vote.usersNotVoted = usersAllowedToVote.filter(u => !userIdsVoted.includes(u.id));
 
 		vote.hasEnded = vote.timeTo < moment().unix();
+
+		// Create results for votes that have ended but still don't have results
+		if (vote.hasEnded) {
+			let results = CR.db.votes.prepare('SELECT results from votes_results where id = ?').get(vote.id);
+			if (results) {
+				results = JSON.parse(results.results);
+			} else {
+				results = {
+					ballots: CR.db.votes.prepare('SELECT user_id, ballot from votes_ballots where vote_id = ?').all(vote.id)
+				};
+
+				if (vote.type === 'jns') {
+					results.reachedQuorum = vote.numBallotsCast / vote.numAllowedToVote >= vote.quorum;
+					results.tally = {
+						'j': 0,
+						'n': 0,
+						's': 0
+					};
+					for (const ballot of results.ballots) {
+						results.tally[ballot.ballot]++;
+					}
+					results.isSindeteno = results.tally.s / vote.numBallotsCast >= .5;
+					if (!vote.majority) {
+						results.hasMajority = results.tally.j > results.tally.n;
+					} else {
+						if (vote.majorityMustBeGreater) {
+							results.hasMajority = results.tally.j >  results.majority * vote.numBallotsCast;
+						} else {
+							results.hasMajority = results.tally.j >= results.majority * vote.numBallotsCast;
+						}
+					}
+					results.isAccepted = !results.isSindeteno && results.hasMajority;
+				} else if (vote.type === 'pr') {
+					results.ballotsStr = results.ballots.map(ballotObj => {
+						const ballotArr = ballotObj.ballot.split('\n').map(x => x.split(','));
+						return ballotArr
+							.map(x => x.map(y => symbols[y]).join('='))
+							.join('>');
+					});
+					try {
+						results.vochoResults = RankedPairs(
+							symbols.substring(0, vote.opts.length), // candidates
+							results.ballotsStr // ballots
+						);
+					} catch (e) {
+						if (e.type === 'TIE_BREAKER_NEEDED') {
+							results.result = 'TIE_BREAKER_NEEDED';
+						} else if (e.type === 'BLANK_BALLOTS') {
+							results.isSindeteno = true;
+						} else {
+							throw e;
+						}
+					}
+					results.isSindeteno = false;
+				} else if (vote.type === 'utv') {
+					results.ballotsStr = results.ballots.map(ballotObj => {
+						return ballotObj.ballot.split('\n').map(x => symbols[x]).join('');
+					});
+					try {
+						results.vochoResults = STV(
+							vote.numWinners, // places
+							symbols.substring(0, vote.opts.length), // candidates
+							results.ballotsStr // ballots
+						);
+					} catch (e) {
+						if (e.type === 'TIE_BREAKER_NEEDED') {
+							results.result = 'TIE_BREAKER_NEEDED';
+						} else if (e.type === 'BLANK_BALLOTS') {
+							results.isSindeteno = true;
+						} else {
+							throw e;
+						}
+					}
+					results.isSindeteno = false;
+				}
+
+				// Insert the results
+				CR.db.votes.prepare('INSERT into votes_results (id, results) values (?, ?)').run(vote.id, JSON.stringify(results));
+			}
+			vote.results = results;
+		}
+		console.log(vote.results);
 
 		return vote;
 	}));
